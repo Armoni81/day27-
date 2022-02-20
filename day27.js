96ae81b6046ff17bf834240c9e3d4d8b525fae3a
Armoni Tigner <atigner2@student.gsu.edu>
2022-02-20 12:38:53 -0500

    solved triangle

---
+++ day27.js
@@ -0,0 +1,16 @@
+/*Given the triangle of consecutive odd numbers:
+1
+3     5
+7     9    11
+13    15    17    19
+21    23    25    27    29
+*/
+
+// holds the odd numbers in the triangle
+
+function rowSumOddNumbers(n) {
+   
+    return n*n*n
+    
+}
+console.log(rowSumOddNumbers(42))
